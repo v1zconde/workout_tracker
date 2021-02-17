1d038cb79405df2ab57613f9639f4158107b1382
rvasquez <rvasquez1@gmail.com>
2021-02-16 21:33:24 -0500

    push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const Mongoose = require("mongoose");
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
